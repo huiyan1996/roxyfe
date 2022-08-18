e5fae730859e2cebbc8fd85bd25fbf209063ab59
kate <kate6450@gmail.com>
2022-08-19 00:07:48 +0800

    init

---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <!-- <topbar /> -->
+    <Nuxt />
+    <!-- <Footer /> -->
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- <Tutorial/> -->
+  <div>
+    Testing
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  head() {
+    return {
+      title: "ROSY"
+    }
+  },
+}
+</script>
