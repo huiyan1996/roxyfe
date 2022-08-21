d8a761295c3d15be1e9aef52d820b4122ed2c568
NIKLAUS_REG\mhlie <regnamint@gmail.com>
2022-08-21 19:03:58 +0800

    draft 3 - navbar

--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,42 @@
 <template>
-    <h1 class="text-center my-3">Header</h1>
+  <b-navbar toggleable class="navbar-bg">
+    <b-navbar-brand class="navbar-logo" href="#"><img src="~/assets/images/rose.png" alt="Logo"><span>ROSYFOXY</span></b-navbar-brand>
+
+    <b-navbar-toggle target="navbar-toggle-collapse">
+      <template #default="{ expanded }">
+        <b-icon v-if="expanded" icon="x-lg"></b-icon>
+        <b-icon v-else icon="list"></b-icon>
+      </template>
+    </b-navbar-toggle>
+
+    <b-collapse id="navbar-toggle-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#">Home</b-nav-item>
+        <b-nav-item href="#">Product</b-nav-item>
+        <b-nav-item href="#">Shop</b-nav-item>
+        <b-nav-item href="#">Contact</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
 </template>
 
 <script>
 </script>
 
 <style lang="scss" scoped>
+.navbar-bg {
+    background: #EBE4D8;
+}
+
+.navbar-logo {
+    height: 40px;
+    img {
+        height: 25px;
+        object-fit: contain;
+        margin-right: 10px;
+    }
+    span {
+        padding-top: 5px;
+    }
+}
 </style>
