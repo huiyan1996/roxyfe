c578772ac875b27fe0fdb9148a31f226038a2ed4
kate <kate6450@gmail.com>
2022-08-25 22:49:48 +0800

    redo menu

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="container-fluid">
-    <b-navbar toggleable class="navbar-bg navbar-mobile">
+    <div class="container navbar">
+      <div class="logo mobileView"><img src="~/assets/images/rose.png" alt="Logo"><span>ROSYFOXY</span></div>
+
+      <a href="javascript:;" class="menuIco mobileView" @click="expanded=!expanded">
+        <b-icon v-if="expanded" icon="x-lg"></b-icon>
+        <b-icon v-else icon="list"></b-icon>
+      </a>
+
+      <div class="overlay" v-if="expanded" @click="expanded=false"></div>
+      <div class="menu" :class="{open: expanded}">
+        <a href="#" class="menuItem">Home</a>
+        <a href="#" class="menuItem">Product</a>
+        <div class="logo pcView"><img src="~/assets/images/rose.png" alt="Logo"><span>ROSYFOXY</span></div>
+        <a href="#" class="menuItem">Shop</a>
+        <a href="#" class="menuItem">Contact</a>
+      </div>
+    </div>
+    <!-- <b-navbar toggleable class="navbar-bg navbar-mobile">
       <b-navbar-brand class="navbar-logo" href="#"><img src="~/assets/images/rose.png" alt="Logo"><span>ROSYFOXY</span></b-navbar-brand>
 
       <b-navbar-toggle target="navbar-toggle-collapse">
@@ -33,47 +50,55 @@
           <b-nav-item href="#">Shop</b-nav-item>
           <b-nav-item href="#">Contact</b-nav-item>
         </b-navbar-nav>
-  </b-navbar>
+  </b-navbar> -->
   </div>
 </template>
 
 <script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      expanded: false,
+    }
+  },
+}
 </script>
 
 <style lang="scss" scoped>
-.navbar-mobile {
-  display: none;
-}
-.navbar-web {
-  display: flex;
-}
+// .navbar-mobile {
+//   display: none;
+// }
+// .navbar-web {
+//   display: flex;
+// }
 
-.navbar-bg {
-    background: #EBE4D8;
-}
+// .navbar-bg {
+//     background: #EBE4D8;
+// }
 
-.navbar-logo {
-    height: 40px;
-    img {
-        height: 25px;
-        object-fit: contain;
-        margin-right: 10px;
-    }
-    span {
-        padding-top: 5px;
-    }
-}
+// .navbar-logo {
+//     height: 40px;
+//     img {
+//         height: 25px;
+//         object-fit: contain;
+//         margin-right: 10px;
+//     }
+//     span {
+//         padding-top: 5px;
+//     }
+// }
 
-.navbar-light .navbar-toggler {
-  border: none;
-}
+// .navbar-light .navbar-toggler {
+//   border: none;
+// }
 
-@media only screen and (max-width: 768px) {
-  .navbar-mobile {
-    display: flex;
-  }
-  .navbar-web {
-    display: none;
-  }
-}
+// @media only screen and (max-width: 768px) {
+//   .navbar-mobile {
+//     display: flex;
+//   }
+//   .navbar-web {
+//     display: none;
+//   }
+// }
 </style>
